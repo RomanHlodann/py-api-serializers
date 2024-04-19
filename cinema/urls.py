5ece361be4bd77ae5afcd973c1b92b3b3ce0671e
RomanHlodann <roman.hlodann@gmail.com>
2024-04-19 12:41:32 +0300

    develop API Serializers

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,25 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet
+)
+
+
+router = routers.DefaultRouter()
+
+router.register(prefix="genres", viewset=GenreViewSet)
+router.register(prefix="actors", viewset=ActorViewSet)
+router.register(prefix="cinema_halls", viewset=CinemaHallViewSet)
+router.register(prefix="movies", viewset=MovieViewSet)
+router.register(prefix="movie_sessions", viewset=MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "cinema"
